23ff863ca7113946c60443c071289616cebdb35f
JULIO ORTIZ <julio.ortiz@mybinxhealth.com>
2023-01-30 07:46:18 -0400

    project configuration tailwind and axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react";
+import axios from 'axios'
+const baseURL = "http://localhost:8000/api/expenses/resume/63d44d1802556286894e3c51";
 function App() {
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+     console.log(response)
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
